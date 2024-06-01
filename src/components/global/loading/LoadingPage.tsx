3d0880d92c45ba9d4ea049c911c7fd60d7be8ab9
Lê Minh Trí <leminhtri11a1@gmail.com>
2024-06-01 23:41:04 +0700

    Update logo in loading page

--- src/components/global/loading/LoadingPage.tsx
+++ src/components/global/loading/LoadingPage.tsx
@@ -1,12 +1,20 @@
+import Image from "next/image";
 import Loading from "./Loading";
 
 export default function LoadingPage() {
     return (
         <div className="relative flex h-screen w-full flex-col items-center justify-start overflow-scroll bg-[#F2F3F3]">
             <div className="flex h-fit w-full flex-row items-center justify-between py-[1rem] px-[4.81rem]">
-                <h1 className="cursor-pointer select-none font-sans text-[1rem] font-bold">
+                {/* <h1 className="cursor-pointer select-none font-sans text-[1rem] font-bold">
                     THE COFFEESHOP
-                </h1>
+                </h1> */}
+                <Image
+                    src="/images/logo.svg"
+                    alt="logo"
+                    width={150}
+                    height={150}
+                    className="cursor-pointer"
+                />
             </div>
             <div className="flex h-full w-full flex-col items-center justify-center">
                 <Loading />
